7bd8b872f6e14885f6ce49f16442c53e882c0b7f
Charles Lehner <cel@celehner.com>
2016-07-01 11:57:08 -0400

    Use niceDate with locale

--- index.jsx
+++ index.jsx
@@ -3,6 +3,6 @@ import * as u from 'patchkit-util'
 
 export default class NiceDate extends React.Component {
   render() {
-    return <span>{u.niceDate(this.props.ts, this.props.ago)}</span>
+    return <span>{u.niceDate(this.props.ts, this.props.ago, this.props.locale)}</span>
   }
 }
